579d2a07f8b449da525986dafcf511b2c110cb91
serferna <setfernet@gmail.com>
2024-06-19 14:00:00 +0200

    Refactor error handling in error.c

--- src/app/error_handlers/error.c
+++ src/app/error_handlers/error.c
@@ -6,7 +6,7 @@
 /*   By: serferna <serferna@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/14 17:34:50 by serferna          #+#    #+#             */
-/*   Updated: 2024/06/14 17:34:51 by serferna         ###   ########.fr       */
+/*   Updated: 2024/06/19 13:59:49 by serferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 void	error(t_stacks *stacks)
 {
-	free_stack(stacks->stack_a);
-	free_stack(stacks->stack_b);
-	free(stacks);
+  end_program(stacks);
 	write(2, "Error\n", 6);
 	exit(1);
 }
